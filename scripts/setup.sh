a5526166e044879a6137b1410ffebad1241c5b74
Marcel Prince <mprince@users.noreply.github.com>
2016-12-02 11:55:00 -0800

    Update Example to 14.04.4

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Allow sudo
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
-sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
+#sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
+#sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 
-apt-get update
+sudo apt-get update
 
-apt-get -y -q install linux-headers-$(uname -r) build-essential dkms nfs-common make gcc software-properties-common
+sudo apt-get -y -q install linux-headers-$(uname -r) build-essential dkms nfs-common make gcc software-properties-common
 
 # Install Vbox Guest Additions
-mkdir /tmp/virtualbox
-mount -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/virtualbox
-sh /tmp/virtualbox/VBoxLinuxAdditions.run
-umount /tmp/virtualbox
-rmdir /tmp/virtualbox
-rm /home/vagrant/*.iso
+sudo mkdir /tmp/virtualbox
+sudo mount -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/virtualbox
+sudo sh /tmp/virtualbox/VBoxLinuxAdditions.run
+sudo umount /tmp/virtualbox
+sudo rmdir /tmp/virtualbox
+sudo rm /home/vagrant/*.iso
 
 # install salt
-add-apt-repository -y ppa:saltstack/salt
-apt-get install -y salt-minion
+sudo add-apt-repository -y ppa:saltstack/salt
+sudo apt-get install -y salt-minion
 
 # upgrade our system
-apt-get -y upgrade
+sudo apt-get -y upgrade
